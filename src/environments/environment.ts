2dab904eae5f662965c1be401c56b081e3dbf298
nek1t-v1zn1uk <1nik.computer1@gmail.com>
2026-07-06 16:27:58 +0300

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:1488/api',
-    serverUrl: 'http://localhost:1488'
+    apiUrl: 'http://localhost:8080/api',
+    serverUrl: 'http://localhost:8080'
 };
